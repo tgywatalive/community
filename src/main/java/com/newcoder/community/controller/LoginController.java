4d1efc11a13acc529b31b3fe1d4b537cbedc7df8
tuogengyu <1607268776@qq.com>
2022-12-12 18:43:42 +0800

    开发注册功能

---
+++ src/main/java/com/newcoder/community/controller/LoginController.java
@@ -0,0 +1,69 @@
+package com.newcoder.community.controller;
+
+import com.newcoder.community.entity.User;
+import com.newcoder.community.service.UserService;
+import com.newcoder.community.util.CommunityConstant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
+
+@Controller
+public class LoginController implements CommunityConstant {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(path = "/register", method = RequestMethod.GET)
+    public String getRegisterPage() {
+        return "/site/register";
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
+    public String getLoginPage() {
+        return "/site/login";
+    }
+
+    /*
+    * 注册
+    * */
+    @RequestMapping(path = "/register", method = RequestMethod.POST)
+    public String register(Model model, User user) {
+        Map<String, Object> map = userService.register(user);
+        if (map == null || map.isEmpty()) {
+            model.addAttribute("msg", "注册成功，我们已经向您的邮箱发送了一封激活邮件，请尽快激活！");
+            model.addAttribute("target", "/index");
+            return "/site/operate-result";
+        } else {
+            // 从service获取map错误提示信息返回到页面上
+            model.addAttribute("usernameMsg", map.get("usernameMsg"));
+            model.addAttribute("passwordMsg", map.get("passwordMsg"));
+            model.addAttribute("emailMsg", map.get("emailMsg"));
+            return "/site/register";
+        }
+    }
+
+    /*
+    * 激活功能
+    * http://localhost:8080/community/activation/101/code
+    * */
+    @RequestMapping(path = "/activation/{userId}/{code}", method = RequestMethod.GET)
+    public String activation(Model model, @PathVariable("userId") int userId, @PathVariable("code") String code) {
+        int result = userService.activation(userId, code);
+        if (result == ACTIVATiON_SUCCESS) {
+            model.addAttribute("msg", "激活成功，您的账号已经可以正常使用了！");
+            model.addAttribute("target", "/login");
+        } else if (result == ACTIVATiON_REPEAT) {
+            model.addAttribute("msg", "无效操作，该账号已经激活过滤！");
+            model.addAttribute("target", "/index");
+        } else {
+            model.addAttribute("msg", "激活失败，您提供的激活码不正确！");
+            model.addAttribute("target", "/index");
+        }
+        return "/site/operate-result";
+    }
+}
---
+++ src/main/java/com/newcoder/community/util/CommunityConstant.java
@@ -0,0 +1,19 @@
+package com.newcoder.community.util;
+
+public interface CommunityConstant {
+
+    /*
+    * 激活成功
+    * */
+    int ACTIVATiON_SUCCESS = 0;
+
+    /*
+    * 重复激活
+    * */
+    int ACTIVATiON_REPEAT = 1;
+
+    /*
+    * 激活失败
+    * */
+    int ACTIVATiON_FAILURE = 2;
+}
